e0527a9b706fdf3ecfa9fc4ef7d34954382e202a
Ben John <bj9@illinois.edu>
2019-02-28 14:43:10 -0600

    initial commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+
+using std::cout;
+using std::endl;
+
+int main() {
+    printf("Hello, World!");
+    cout<<endl;
+    cout<<"Ben John the losser"<<endl;
+    return 0;
+}
